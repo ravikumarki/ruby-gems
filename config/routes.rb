153089b47a1a90a604223d24e8e4e2d03ea92127
ravikumarki <ravikumarki@github.com>
2022-05-03 10:46:00 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users
+  resources :courses
+  root 'home#index'
+  #root 'static_pages#landing_page'
+  #get 'static_pages/landing_page'
+  #get 'static_pages/privacy_policy'
+  get "privacy_policy",to:'static_pages#privacy_policy'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ db/migrate/20220429094540_add_user_tocourses.rb
@@ -0,0 +1,6 @@
+class AddUserTocourses < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :courses, :user, null: false, foreign_key: true
+  end
+
+end
